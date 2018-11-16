e193a392530018ccb6509b1f76d21f9b1742ed9a
Warning <36586296+rookieman88@users.noreply.github.com>
2018-11-16 20:47:36 +0900

    Update prefix.js

--- prefix.js
+++ prefix.js
@@ -3,6 +3,17 @@ const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
 
+ if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("ㄴㄴ");
+ if(!args[0] || args[0 == "help"]) return message.reply("사용법: ~prefix <명령어>");
+ 
+ let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"))
+ 
+ prefixes[message.guild.id] = {
+  prefixes: args[0]
+ };
+ 
+ fs.writeFile(
+ 
 }
 
 module.exports.help = {

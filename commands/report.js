ca26a4aea1387810adc66d5a7f0d7ff0e5913208
Warning <36586296+rookieman88@users.noreply.github.com>
2018-11-17 09:52:12 +0900

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,1 +1,27 @@
+const Discord = require("discord.js");
 
+ module.exports.run = async (bot, message, args) => {
+		
+		let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+		if(!rUser) return message.channel.send("유저를 찾을 수 없습니다.");
+		let reason = args.join(" ").slice(22);
+		
+		
+		let reportEmbed = new Discord.RichEmbed()
+		.setDescription("신고")
+		.setColor("#15f153")
+		.addField("신고한 유저", `${rUser} ID: ${rUser.id}`)
+		.addField("시각", message.createdAt)
+		.addField("사유", reason);
+		
+		let reportschannel = message.guild.channels.find(`name`, "신고");
+		if(!reportschannel) return message.channel.send("채널을 찾을 수 없습니다. 신고 체널을 만들어주세요!");
+		
+		 message.delete().catch(O_o=>{});
+		 reportschannel.send(reportEmbed);
+		return;
+	}
+	
+module.exports.help = {
+	name: "신고"
+}

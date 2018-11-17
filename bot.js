60abf5385aa837923edfa08989787a68b59b26f9
Warning <36586296+rookieman88@users.noreply.github.com>
2018-11-17 09:52:56 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,33 @@
 const Discord = require('discord.js');
 const botconfig = require("./botconfig.json");
-
+const fs = require("fs");
 const bot = new Discord.Client();({disableEveryone: true});
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+	
+	if(err) console.log(err);
+	
+	let jsfile = files.filter(f => f.split(".").pop() === "js")
+	if(jsfile.length <= 0){
+		console.log("커멘드를 찾알 수 없습니다");
+		return;
+	}
+	
+	jsfile.forEach((f, i) =>{
+		let props = require(`./commands/${f}`);
+		console.log(`${f} 실행됨`);
+		bot.commands.set(props.help.name, props);
+	});
+});
 
 console.log('봇 실행 완료');
 
 bot.on('ready', async () => {
 	bot.user.setActivity("~워터야 도움 ㄱㄱ");
 });
+
+
 
 bot.on("message", async message => {
 	if(message.author.bot) return;
@@ -18,29 +38,32 @@ bot.on("message", async message => {
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
 	
-	if(cmd === `${prefix}추방`){
+	let commandfile = bot.commands.get(cmd.slice(prefix.length));
+	if(commandfile) commandfile.run(bot,message,args);
+	
+//	if(cmd === `${prefix}추방`){
 		
-		let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-		if(!kUser) return message.channel.send("유저를 찾을 수 없습니다.");
-		let kReason = args.join(" ").slice(22);
-		if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("권한이 없습니다!");
-		if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("그 사람을 추방할 수 없습니다!");
+		//let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+		//if(!kUser) return message.channel.send("유저를 찾을 수 없습니다.");
+		//let kReason = args.join(" ").slice(22);
+		//if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("권한이 없습니다!");
+		//if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("그 사람을 추방할 수 없습니다!");
 		
-		let kickEmbed = new Discord.RichEmbed()
-		.setDescription("추방됨")
-		.setColor("#e56b00")
-		.addField("추방된 유저", `${kUser} ID: ${kUser.id}`)
-		.addField("관리자", `<@${message.author.id}>`)
-		.addField("시각", message.createdAt)
-		.addField("사유", kReason);
+		//let kickEmbed = new Discord.RichEmbed()
+		//.setDescription("추방됨")
+		//.setColor("#e56b00")
+		//.addField("추방된 유저", `${kUser} ID: ${kUser.id}`)
+		//.addField("관리자", `<@${message.author.id}>`)
+		//.addField("시각", message.createdAt)
+		//.addField("사유", kReason);
 		
-		let kickChannel = message.guild.channels.find(`name`, "경고");
-		if(!kickChannel) return message.channel.send("채널을 찾을 수 없습니다. 경고 체널을 만들어주세요!");
+		//let kickChannel = message.guild.channels.find(`name`, "경고");
+		//if(!kickChannel) return message.channel.send("채널을 찾을 수 없습니다. 경고 체널을 만들어주세요!");
 		
-		message.guild.member(kUser).kick(kReason);
-		kickChannel.send(kickEmbed);
-		return;
-	}
+		//message.guild.member(kUser).kick(kReason);
+		//kickChannel.send(kickEmbed);
+		///return;
+	//}
 	
 	 if(cmd === `${prefix}차단`){
 
@@ -68,27 +91,27 @@ bot.on("message", async message => {
     return;
   }
 	
-	if(cmd === `${prefix}신고`){
-		
-		let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-		if(!rUser) return message.channel.send("유저를 찾을 수 없습니다.");
-		let reason = args.join(" ").slice(22);
-		
-		
-		let reportEmbed = new Discord.RichEmbed()
-		.setDescription("신고")
-		.setColor("#15f153")
-		.addField("신고한 유저", `${rUser} ID: ${rUser.id}`)
-		.addField("시각", message.createdAt)
-		.addField("사유", reason);
-		
-		let reportschannel = message.guild.channels.find(`name`, "신고");
-		if(!reportschannel) return message.channel.send("채널을 찾을 수 없습니다. 신고 체널을 만들어주세요!");
-		
-		 message.delete().catch(O_o=>{});
-		 reportschannel.send(reportEmbed);
-		return;
-	}
+	//if(cmd === `${prefix}신고`){
+		//
+		//let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+		//if(!rUser) return message.channel.send("유저를 찾을 수 없습니다.");
+		//let reason = args.join(" ").slice(22);
+		//
+		//
+		//let reportEmbed = new Discord.RichEmbed()
+		//.setDescription("신고")
+		//.setColor("#15f153")
+		//.addField("신고한 유저", `${rUser} ID: ${rUser.id}`)
+		//.addField("시각", message.createdAt)
+		//.addField("사유", reason);
+		//
+		//let reportschannel = message.guild.channels.find(`name`, "신고");
+		//if(!reportschannel) return message.channel.send("채널을 찾을 수 없습니다. 신고 체널을 만들어주세요!");
+		//
+		// message.delete().catch(O_o=>{});
+		// reportschannel.send(reportEmbed);
+		//return;
+	//}
 	
 	var msg = message.content.toUpperCase();
 

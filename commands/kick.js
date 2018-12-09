75915d29a98db91f47a5e383ad19d180563512b9
Warning <36586296+rookieman88@users.noreply.github.com>
2018-12-09 10:44:30 +0900

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 		
 let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-		if(!kUser) return message.channel.send("유저를 찾을 수 없습니다.");
+		if(!kUser) return message.channel.send("사용법 : ~추방 @사람이름 사유);
 		let kReason = args.join(" ").slice(22);
 		if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("권한이 없습니다!");
 		if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("그 사람을 추방할 수 없습니다!");
@@ -16,7 +16,7 @@ let kUser = message.guild.member(message.mentions.users.first() || message.guild
 		.addField("시각", message.createdAt)
 		.addField("사유", kReason);
 		
-		let kickChannel = message.guild.channels.find(`name`, "경고");
+		let kickChannel = message.guild.channels.find(`name`, "");
 		if(!kickChannel) return message.channel.send("채널을 찾을 수 없습니다. 경고 체널을 만들어주세요!");
 		
 		message.guild.member(kUser).kick(kReason);

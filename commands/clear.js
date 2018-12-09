06a90e42858c7e6cb9bdc8af690443cb7d057efc
Warning <36586296+rookieman88@users.noreply.github.com>
2018-12-09 10:42:18 +0900

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("권한이 없습니다.");
-  if(!args[0]) return message.channel.send("?");
+  if(!args[0]) return message.channel.send("사용법 : ~삭제 [삭제하고싶은 메시지 수]");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`${args[0]} 개의 매세지가 삭제되었습니다.`).then(msg => msg.delete(2000));
 });

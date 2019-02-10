1c7e2f40ba9445562e159bf946959bb833c8be33
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-02-10 22:06:44 +0900

    Create mute.js

---
+++ commands/mute.js
@@ -0,0 +1,34 @@
+/*
+
+const Discord = require("discord.js");
+const ms = require("ms");
+
+module.exports.run = async (bot, message, args) => {
+
+  //!tempmute @user 1s/m/h/d
+
+  let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!tomute) return message.reply("사용법 : ~뮤트 @사람이름");
+  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("그 사람을 뮤트시킬수 없습니다");
+  let muterole = message.guild.roles.find(`name`, "Muted");
+  
+  let mutetime = args[1];
+  if(!mutetime) return message.reply("You didn't specify a time!");
+
+  await(tomute.addRole(muterole.id));
+  message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+
+  setTimeout(function(){
+    tomute.removeRole(muterole.id);
+    message.channel.send(`<@${tomute.id}> has been unmuted!`);
+  }, ms(mutetime));
+
+
+//end of module
+}
+
+module.exports.help = {
+  name: "뮤트"
+}
+
+*/

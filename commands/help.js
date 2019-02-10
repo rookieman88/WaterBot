02882c6328c179916f3144fac9560f52e6544608
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-02-10 21:10:27 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,31 +6,16 @@ module.exports.run = async (bot, message, args) => {
  let botembed = new Discord.RichEmbed()
         .setTitle("도움말")
         .setColor("#33cc33")
-        .addField("기본 도움말", "기본적인 명령어를 알려드립니다.")
-        .addField("관리자 도움말", "관리자용 명령어를 알려드립니다.")
-        .addField("미니게임 도움말", "미니게임에 대하여 알려드립니다.")
-	.addField("꼭 필요한 체널입니다.", "#인사 #경고 #신고");
-        return message.channel.send(botembed);
-
-  let normalbotembed = new Discord.RichEmbed()
-         .setTitle("기본 도움말")
-        .setColor("#33cc33")
+        .addField("기본 도움말", "기본 명령어 입니다.")
         .addField("워터를 부르는 방법", "~")
         .addField("명령어", "곧 개발됩니다..")
-        .addField("워터봇 개발자", "NotOasics#5074")
-        return message.channel.send(normalbotembed);
-
-  let adminbotembed = new Discord.RichEmbed()
-         .setTitle("관리자 도움말")
-        .setColor("#33cc33")
+        .addField("관리자 도움말", "관리자 명령어 도움말 입니다.")
         .addField("명령어", "신고 경고 차단 추방 삭제 뮤트 등..")
-        return message.channel.send(adminbotembed);
-
-  let gamebotembed = new Discord.RichEmbed()
-         .setTitle(" 도움말")
-        .setColor("#33cc33")
+        .addField("게임 도움말", "게임 명령어 입니다.")
         .addField("명령어", "곧 개발됩니다..")
-        return message.channel.send(gamebotembed);
+        .addField("워터봇 개발자", "NotOasics#5074")
+	.addField("꼭 필요한 체널입니다.", "#인사 #경고 #신고");
+        return message.channel.send(botembed);
 
 
 });

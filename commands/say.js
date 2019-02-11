2d1c20f430e8f446b4d2caef8ecb21eb07c37d0a
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-02-11 16:40:06 +0900

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
       const sayMessage = args.join(" ");
+      message.delete().catch();
       message.channel.send(sayMessage);
 
 }

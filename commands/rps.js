74524bdf2eed7fd3735bd7076fdffb5e0e7d9bc6
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-02-11 20:18:02 +0900

    Update rps.js

--- commands/rps.js
+++ commands/rps.js
@@ -8,17 +8,37 @@ module.exports.run = async (bot, message, args) =>{
 
   if (args[0]) {
         // get user choice && user choice
-        let computer_choice = rand(0,2);
+        let ai_choice = rand(0,2);
         let user_choice = args[0] == "바위" ? 1 : args[0] == "보" ? 2 : 0;
 
+      
+      if (ai_choice == 0) {
+          message.channel.send(`워터는 가위 :v: 를 냈다!`);
+      }
+      else if (ai_choice == 1) {
+          message.channel.send(`워터는 바위 :fist: 를 냈다!`);
+      } else {
+          message.channel.send(`워터는 보 :hand_splayed: 를 냈다!`); 
+      }
+      
+      
+      if (user_choice == 0) {
+          message.channel.send(`<@${message.author.id}>은(는) 가위 :v: 를 냈다!`);
+      }
+      else if (user_choice == 1) {
+          message.channel.send(`<@${message.author.id}>은(는) :fist: 를 냈다!`);
+      } else {
+          message.channel.send(`<@${message.author.id}>은(는) 보 :hand_splayed: 를 냈다!`); 
+      }
+      
         // if their choices are same its a draw :D
-        if (computer_choice == user_choice) {
-          message.reply("무승부!");
+        if (ai_choice == user_choice) {
+          message.reply("무승부를 했다!");
         }
-        else if (computer_choice < user_choice || computer_choice == 0 && user_choice == 2) {
-          message.reply("님 졌음 ㅅㄱ");
+        else if (ai_choice < user_choice || ai_choice == 0 && user_choice == 2) {
+          message.reply(`<@${message.author.id}>가 졌다!`);
         } else {
-          message.reply("님이 이겼음");
+          message.reply(`<@${message.author.id}>가 이겼다!`);
         }
       }
 

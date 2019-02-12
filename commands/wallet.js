92152a35296ff32ad03447ba0d8e7f0d276e623e
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-02-12 21:26:16 +0900

    Create wallet.js

---
+++ commands/wallet.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+let coins = JSON.parse(fs.readFileSync(".saves/coins.json", "utf8"));
+
+module.exports.run = async (bot, message, args) => {
+
+
+  let user = message.author.id
+  let usercoin = coins[user.id].warns;
+  
+  if(!coins[user.id]) coins[user.id] = {
+    coins: 0
+  };
+  
+
+
+  message.reply(`<@${user.id}>님은 ${usercoin} 만큼의 돈을 가지고있다!`);
+
+}
+
+module.exports.help = {
+  name: "지갑"
+}

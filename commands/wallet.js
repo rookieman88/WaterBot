a1a7019a87b7f26455b99b317b14111d0784d714
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-02-12 21:28:50 +0900

    Update wallet.js

--- commands/wallet.js
+++ commands/wallet.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const ms = require("ms");
-let coins = JSON.parse(fs.readFileSync(".saves/coins.json", "utf8"));
+let coins = JSON.parse(fs.readFileSync("./saves/coins.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
 

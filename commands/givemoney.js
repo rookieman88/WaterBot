3a116fc282fd4f4af1e6653e8410de17cee25d06
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-02-13 19:28:55 +0900

    Update givemoney.js

--- commands/givemoney.js
+++ commands/givemoney.js
@@ -5,10 +5,12 @@ let coins = JSON.parse(fs.readFileSync("./saves/coins.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
 
-if(!coins[message.author.id]) coins[message.author.id] = {
+let user = message.author    
+
+if(!coins[user.id]) coins[user.id] = {
     coins: 100
   };
- coins[message.author.id].coins ++;
+ coins[user.id].coins + 100;
 
   
   message.reply("용돈 100원이 지급되었다!");

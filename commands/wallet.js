3d05ef79f1cebda68ee22e8683636f76cab2f995
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-02-14 16:27:19 +0900

    Update wallet.js

--- commands/wallet.js
+++ commands/wallet.js
@@ -1,15 +1,21 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const ms = require("ms");
-let coins = JSON.parse(fs.readFileSync("./saves/coins.json", "utf8"));
+let coins = require("../saves/coins.json");
 
 module.exports.run = async (bot, message, args) => {
 
-  let user = message.author
-  let usercoin = coins[user.id].coins;
-
-  message.channel.send(`<@${message.author.id}>님은 ${usercoin} 만큼의 돈을 가지고있다!`);
-
+  if(!coins[message.author.id]){
+   coins[message.authorid] = {
+     coins: 0
+   };
+    
+  }
+let usercoin = coins[message.author.id].coins;
+  
+  message.channel.send(`당신은 ${usercoin} 만큼의 돈을 가지고 있습니다!`);
+  
+  
 }
 
 module.exports.help = {

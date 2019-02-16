ad025a36a1127de214472ff05401584eda85b38e
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-02-16 12:35:36 +0900

    Update givemoney.js

--- commands/givemoney.js
+++ commands/givemoney.js
@@ -1,13 +1,16 @@
 const Discord = require("discord.js");
 const fs = require("fs");
-let Coin = JSON.parse(fs.readFileSync("./saves/coins.json", "utf8"));
+
 
 module.exports.run = async (bot, message, args) => {
 
 
+	message.channel.send("용돈 100원이 지급되었습니다! 잔액을 확인하려면 ~지갑 을 입력하세요");
+	
   let YongDong = 100;
 
-  message.channel.send("용돈 100원이 지급되었습니다! 잔액을 확인하려면 ~지갑 을 입력하세요");
+ 
+     let Coin = JSON.parse(fs.readFileSync("./saves/coins.json", "utf8"));
 	
 	if(!Coin[message.author.id]) Coin[message.author.id] = {
 	 Coin: 0

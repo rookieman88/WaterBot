8b433f49b5f25455653d525123cb949358224770
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-02-16 12:00:44 +0900

    Update givemoney.js

--- commands/givemoney.js
+++ commands/givemoney.js
@@ -1,27 +1,30 @@
 const Discord = require("discord.js");
 const fs = require("fs");
-let coins = JSON.parse(fs.readFileSync("./saves/coins.json", "utf8"));
+let Coin = JSON.parse(fs.readFileSync("./saves/coins.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
-	
-
-  let sCoins = coins[message.author.id].coins;
-  let yongdong = 100;
-
-	
-  coins[message.author.id] = {
-    coins: sCoins + parseInt(yongdong)
-  };
 
 
-  fs.writeFile("./saves/coins.json", JSON.stringify(coins), (err) => {
+  let YongDong = 100;
+
+
+	if(!Coin[message.author.id]) Coin[message.author.id] = {
+	 Coin: 0
+ };
+
+	Coin[message.author.id] = {
+			Coin: Coin[message.author.id].Coin + (YongDong)
+	};
+
+
+  fs.writeFile("./saves/coins.json", JSON.stringify(Coin), (err) => {
     if(err) cosole.log(err)
   });
 
  let msgembed = new Discord.RichEmbed()
  .setColor("#0000FF")
  .addField("용돈 100원이 지급되었습니다!", "잔액을 확인하려면 ~지갑 을 입력하세요!");
-    
+
  message.channel.send(msgembed)
 
 }

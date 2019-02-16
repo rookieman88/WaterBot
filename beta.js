7edf6472b203d99975297aa18dfa91306dee2e61
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-02-17 08:52:07 +0900

    Update beta.js

--- beta.js
+++ beta.js
@@ -167,11 +167,20 @@ bot.on("message", async message => {
 		
 	
 	
-	/*
+	
        
        if(message.author.bot) return;
 	var msg = message.content.toUpperCase();
-
+	
+	if(msg === '~돈내놔'){
+       let botembed = new Discord.RichEmbed()
+        .setColor("#33cc33")
+	.addField("용돈 100원이 지급되었습니다!", "잔액을 확인하려면 ~지갑 입력 ");
+        
+        return message.channel.send(botembed);
+    }
+	
+/*
    if(msg === '워터야 도움'){
        let botembed = new Discord.RichEmbed()
         .setDescription("도움말")

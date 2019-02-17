701c44eff8a50aff7ffd1cfb5a469f961be913ca
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-02-17 09:12:07 +0900

    Update beg.js

--- commands/beg.js
+++ commands/beg.js
@@ -1,37 +1,40 @@
 const Discord = require("discord.js");
 const fs = require("fs");
-let coins = require("../saves/coins.json");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!coins[message.author.id]){
+  let Coin = JSON.parse(fs.readFileSync("./saves/coins.json", "utf8"));
+  
+  if(!Coin[message.author.id]){
     return message.reply("님도 돈이 없는데?")
   }
 
   let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
 
-  if(!coins[pUser.id]){
-    coins[pUser.id] = {
-      coins: 0
+  if(!Coin[pUser.id]){
+    Coin[pUser.id] = {
+      Coin: 0
     };
   }
 
-  let pCoins = coins[pUser.id].coins;
-  let sCoins = coins[message.author.id].coins;
+  let pCoins = Coin[pUser.id].Coin;
+  let sCoins = Coin[message.author.id].Coin;
 
-  if(sCoins < args[0]) return message.reply("그만큼 돈은 있음?");
+  if(sCoins < args[1]) return message.reply("그만큼 돈은 있음?");
 
-  coins[message.author.id] = {
-    coins: sCoins - parseInt(args[1])
+    message.channel.send(` ${pUser}한테 ${args[1]} 만큼 기부한거 맞죠? 환불 안됨 `);
+  
+  Coin[message.author.id] = {
+    Coin: sCoins - parseInt(args[1])
   };
 
-  coins[pUser.id] = {
-    coins: pCoins + parseInt(args[1])
+  Coin[pUser.id] = {
+    Coin: pCoins + parseInt(args[1])
   };
 
-  message.channel.send(` ${pUser}한테 ${args[1]} 만큼 기부한거 맞죠? 환불 안됨 `);
 
-  fs.writeFile("./saves/coins.json", JSON.stringify(coins), (err) => {
+
+  fs.writeFile("./saves/coins.json", JSON.stringify(Coin), (err) => {
     if(err) cosole.log(err)
   });
 

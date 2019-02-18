ba556e89e7567aa1e3db24b79fe1892c29927014
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-02-18 15:50:39 +0900

    Update givemoney.js

--- commands/givemoney.js
+++ commands/givemoney.js
@@ -7,21 +7,20 @@ module.exports.run = async (bot, message, args) => {
 	
   let YongDong = 100;
 
- 
-     let Coin = JSON.parse(fs.readFileSync("./saves/coins.json", "utf8"));
+	superagent.get("https://api.jsonbin.io/b/5c62c948ad5128320af85de0/latest").then((res) => {
+		let WatCoin = res.body;
+
 	
-	if(!Coin[message.author.id]) Coin[message.author.id] = {
-	 Coin: 0
+	if(!WatCoin[message.author.id]) WatCoin[message.author.id] = {
+	 WatCoin: 0
  };
 
-	Coin[message.author.id] = {
-			Coin: Coin[message.author.id].Coin + (YongDong)
+	WatCoin[message.author.id] = {
+			WatCoin: WatCoin[message.author.id].WatCoin + (YongDong)
 	};
 
 
-  fs.writeFile("./saves/coins.json", JSON.stringify(Coin), (err) => {
-    if(err) cosole.log(err)
-  });
+ superagent.put("https://api.jsonbin.io/b/5c62c948ad5128320af85de0").send(WatCoin).catch((err) => console.log(err));
 
 }
 

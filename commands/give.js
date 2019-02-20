a9ced00be7cc9a7fd710107e6f6eb05f8f497ecd
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-02-20 18:32:28 +0900

    Update give.js

--- commands/give.js
+++ commands/give.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
        let WatCoin;
 	superagent.get("https://api.jsonbin.io/b/5c62c948ad5128320af85de0/latest").then((res) => {
 		WatCoin = res.body;
-  
+
   if(!WatCoin[message.author.id]){
     return message.reply("님도 돈이 없는데?")
   }
@@ -26,7 +26,7 @@ module.exports.run = async (bot, message, args) => {
   if(sCoins < args[1]) return message.reply("그만큼 돈은 있음?");
 
     message.channel.send(` ${pUser}한테 ${args[1]} 만큼 기부한거 맞죠? 환불 안됨 `);
-  
+
   WatCoin[message.author.id] = {
     WatCoin: sCoins - parseInt(args[1])
   };
@@ -38,7 +38,7 @@ module.exports.run = async (bot, message, args) => {
 
 
  superagent.put("https://api.jsonbin.io/b/5c62c948ad5128320af85de0").send(WatCoin).catch((err) => console.log(err));
-  }
+};
 }
 
 module.exports.help = {

347db5ec22d3de9c50395af2909d5b2714b8ac0e
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-02-20 17:35:21 +0900

    Update beg.js

--- commands/beg.js
+++ commands/beg.js
@@ -3,42 +3,41 @@ const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
 
-  let Coin = JSON.parse(fs.readFileSync("./saves/coins.json", "utf8"));
+       let WatCoin;
+	superagent.get("https://api.jsonbin.io/b/5c62c948ad5128320af85de0/latest").then((res) => {
+		WatCoin = res.body;
   
-  if(!Coin[message.author.id]){
+  if(!WatCoin[message.author.id]){
     return message.reply("님도 돈이 없는데?")
   }
 
   let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
 
-  if(!Coin[pUser.id]){
-    Coin[pUser.id] = {
-      Coin: 0
+  if(!WatCoin[pUser.id]){
+    WatCoin[pUser.id] = {
+      WatCoin: 0
     };
   }
 
-  let pCoins = Coin[pUser.id].Coin;
-  let sCoins = Coin[message.author.id].Coin;
+  let pCoins = WatCoin[pUser.id].WatCoin;
+  let sCoins = WatCoin[message.author.id].WatCoin;
 
   if(sCoins < args[1]) return message.reply("그만큼 돈은 있음?");
 
     message.channel.send(` ${pUser}한테 ${args[1]} 만큼 기부한거 맞죠? 환불 안됨 `);
   
-  Coin[message.author.id] = {
-    Coin: sCoins - parseInt(args[1])
+  WatCoin[message.author.id] = {
+    WatCoin: sCoins - parseInt(args[1])
   };
 
-  Coin[pUser.id] = {
-    Coin: pCoins + parseInt(args[1])
+  WatCoin[pUser.id] = {
+    WatCoin: pCoins + parseInt(args[1])
   };
 
 
 
-  fs.writeFile("./saves/coins.json", JSON.stringify(Coin), (err) => {
-    if(err) cosole.log(err)
-  });
-
-
+ superagent.put("https://api.jsonbin.io/b/5c62c948ad5128320af85de0").send(WatCoin).catch((err) => console.log(err));
+  }
 }
 
 module.exports.help = {

5b2f02f3d05d8b6edc74177f0da94e7357373533
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-02-23 22:23:10 +0900

    Update dice.js

--- commands/dice.js
+++ commands/dice.js
@@ -9,8 +9,6 @@ module.exports.run = async (bot, message, args) => {
 	superagent.get("https://api.jsonbin.io/b/5c6e98737bded36fef1b5240/latest").then((res) => {
 		WatCoin = res.body;
 
-    let yongdong;
-
   if(!WatCoin[message.author.id]){
     WatCoin[message.author.id] = {
       WatCoin: 0
@@ -18,36 +16,45 @@ module.exports.run = async (bot, message, args) => {
   }
     
 		let ranNum = Math.floor(Math.random() * (5)) + 0;
+	
+		let coinAmtMent;
+if (ranNum === 0) {
+      coinAmtMent = 0;
+  } else if (ranNum === 1) {
+      coinAmtMent = 10;
+  } else if (ranNum === 2) {
+      coinAmtMent = 20;
+  } else if (ranNum === 3) {
+      coinAmtMent = 30;
+  } else if (ranNum === 4) {
+      coinAmtMent = 40;
+  } else if (ranNum === 5) {
+      coinAmtMent = 50;
+  }
 		
     if (ranNum === 0) {
       message.reply(`:one: (1) 이 나왔다! (+ 0 Coins)`);
-     yongdong: 0
 }
   else if (ranNum === 1) {
     message.reply(`:two: (2) 가 나왔다! (+ 10 Coins)`);
-    yongdong: 10
   }
     else if (ranNum === 2) {
       message.reply(`:three: (3) 이 나왔다! (+20 Coins)`);
-      yongdong: 20
     }
     else if (ranNum === 3) {
       message.reply(`:four: (4) 가 나왔다! (+30 Coins)`);
-      yongdong: 30
     }
         else if (ranNum === 4) {
       message.reply(`:five: (5) 가 나왔다! (+40 Coins)`);
-      yongdong: 40
         }
         else if (ranNum === 5) {
       message.reply(`:six: (6) 이 나왔다! (+50 Coins)`);
-      yongdong: 50
   };
 
 
 
   WatCoin[message.author.id] = {
-    WatCoin: WatCoin[message.author.id].WatCoin + yongdong
+    WatCoin: WatCoin[message.author.id].WatCoin + (coinAmtMent / 1)
   };
 
 

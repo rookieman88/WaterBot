471440bf6434ddee3cc25d32d97297e94be311a4
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-02-24 13:31:32 +0900

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -12,17 +12,18 @@ module.exports.run = async (bot, message, args) => {
       coinAmtMent = 30;
   } else if (coinAmt === 1) {
       coinAmtMent = 10;
+  };
 
-  if (coinResult === 0) {
+  if (coinAmt === 0) {
     message.channel.send(`동전을 던집니다! 결과는?
     앞이네요! +30 Coins`);
       WatCoin[message.author.id] = {
           WatCoin: WatCoin[input.author.id].WatCoin + (coinAmtMent / 1)
-      };
-  } else if (coinResult === 1) {
+      }
+  } else if (coinAmt === 1) {
           message.channel.send(`동전을 던집니다! 결과는?
     뒤네요.. -10 Coins`);
-      UsersCoin[message.author.id] = {
+      WatCoin[message.author.id] = {
           WatCoin: WatCoin[message.author.id].WatCoin - (coinAmtMent / 1)
       };
   superagent.put("https://api.jsonbin.io/b/5c6e98737bded36fef1b5240").send(WatCoin).catch((err) => console.log(err));

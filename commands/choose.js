e40887ca8f8a65481a541932054ff02f8c3ac886
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-02-27 18:35:32 +0900

    Update choose.js

--- commands/choose.js
+++ commands/choose.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (mu, input, pars) => {
-    let say = pars.join(" ").slice(0);
+module.exports.run = async (bot, message, args) => {
+    let say = args.join(" ").slice(0);
     let choses = say.split(" ");
     let ranChose = Math.floor(Math.random() * (choses.length) + 0);
-    input.channel.send(`"${choses[ranChose]}"~`)
+    message.channel.send(`"${choses[ranChose]}"~`)
 };
 
 module.exports.help = {

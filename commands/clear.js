27f3bbb006e2de4598d2dadb7168d718fa20fdae
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-03-01 11:15:20 +0900

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -4,7 +4,8 @@ module.exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("권한이 없습니다.");
   if(!args[0]) return message.channel.send("사용법 : ~삭제 [삭제하고싶은 메시지 수]");
-  message.channel.bulkDelete(args[0] + 1).then(() => {
+  let clearcount = (args[0]) + 1
+  message.channel.bulkDelete(clearcount).then(() => {
   message.channel.send(`${args[0]} 개의 매세지가 삭제되었습니다.`).then(msg => msg.delete(2000));
 });
 

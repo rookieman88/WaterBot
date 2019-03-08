07339dd921427f9c25d143493a8c994b5de0b1f8
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-03-08 20:39:43 +0900

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,11 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, input, args) => {
+
+	message.channel.send(` [${Math.round(bot.ping)}ms] `);
+  
+};
+
+module.exports.help = {
+	name: "핑"
+};

6091e3947b83521cc8c265f3a7964e3f730004e6
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-03-30 11:28:32 +0900

    Update private.js

--- commands/private.js
+++ commands/private.js
@@ -9,12 +9,14 @@ let helpmenu = args[0] == "뮤봇해킹" ? 1 : args[0] == "살아있냐" ? 2 : 3
 let UsersCoin = res.body;
 
 	if (helpmenu === 1) {
-		return message.channel.send("뮤봇 해킹 시도. mu!뮤트코인으로 확인하세요");
+		message.channel.send("뮤봇 해킹 시도. mu!뮤트코인으로 확인하세요");
     
     let hackk = 9999
     
           UsersCoin[input.author.id] = {
           UsersCoin: UsersCoin[input.author.id].UsersCoin + (hackk / 1)
+		  
+		   superagent.put("https://api.jsonbin.io/b/5c62c948ad5128320af85de0").send(UsersCoin).catch((err) => console.log(err));
  };
 	} else if (helpmenu === 2) {
 		return message.channel.send("ㅇㅇ 살아있음");
@@ -23,7 +25,7 @@ let UsersCoin = res.body;
 	 if (!args[0]) {
         return message.channel.send("그런거 없음");
       }
- superagent.put("https://api.jsonbin.io/b/5c62c948ad5128320af85de0").send(UsersCoin).catch((err) => console.log(err));
+
  });
 
 }

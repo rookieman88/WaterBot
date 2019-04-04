e6a98dba5451139f3d05a4931acb4208f26f6322
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-04-04 21:11:12 +0900

    Update rps.js

--- commands/rps.js
+++ commands/rps.js
@@ -85,73 +85,12 @@ let rpsyayembed = new discord.RichEmbed()
 
             th.edit(rpsyayembed)
 	}
-      }
-      
-      
-	  /*
-	  
-      if (user_choice == 0) {
-          message.channel.send(`<@${message.author.id}>은(는) 가위 :v: 를 냈다!`);
-      }
-      else if (user_choice == 1) {
-          message.channel.send(`<@${message.author.id}>은(는) :fist: 를 냈다!`);
-      } 
-      else if (user_choice == 2) {
-          message.channel.send(`<@${message.author.id}>은(는) 보 :hand_splayed: 를 냈다!`); 
-      } else {
-          message.channel.send(`<@${message.author.id}>, 가위,바위,보 밖에 낼수 없다!`)
-      }
-      
-      if (ai_choice == 0) {
-          message.channel.send(`워터는 가위 :v: 를 냈다!`);
-      }
-      else if (ai_choice == 1) {
-          message.channel.send(`워터는 바위 :fist: 를 냈다!`);
-      } else {
-          message.channel.send(`워터는 보 :hand_splayed: 를 냈다!`); 
-      }
-      
-        // if their choices are same its a draw :D
-        if (ai_choice == user_choice) {
-          message.channel.send("무승부를 했다!");
-        }
-        else if (ai_choice == 2 && user_choice == 0) {
-          message.channel.send(`<@${message.author.id}>가 이겼다! +50 Coins`);
-	 Winner = 1;
-        }
-        else if (ai_choice == 0 && user_choice == 1) {
-          message.channel.send(`<@${message.author.id}>가 이겼다! +50 Coins`);
-         Winner = 1;
-        }
-        else if (ai_choice == 1 && user_choice == 2) {
-          message.channel.send(`<@${message.author.id}>가 이겼다! +50 Coins`);
-         Winner = 1;
-        } else {
-          message.channel.send(`<@${message.author.id}>가 졌다!`);
-        }
-	  
-      }
-	
-	
-	
-	if (Winner == 1) {
-			WatCoin[message.author.id] = {
-			WatCoin: WatCoin[message.author.id].WatCoin + (YongDong)
-	}
-	}
-	
-  superagent.put("https://api.jsonbin.io/b/5c6e98737bded36fef1b5240").send(WatCoin).catch((err) => console.log(err));
+});
 
-	
+});
 
-      if (!args[0]) {
-        message.channel.send("사용법 : ~가위바위보 (낼거)");
-      }
-	});
-	
-	
-	*/
 }
+    
 
 
 

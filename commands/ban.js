19d717d55c62bb26c044f794e5a515976c712f1e
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-04-06 18:11:39 +0900

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -18,7 +18,7 @@ module.exports.run = async (bot, message, args) => {
     let incidentchannel = message.guild.channels.find(`name`, "경고");
 	 message.channel.send(`
                       
-${bUser} 가 경고되었습니다.
+${bUser} 가 차단되었습니다.
 사유는 [ ${bReason} ] 입니다.
 		      
 		      `)

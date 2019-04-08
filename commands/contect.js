f6c21fb3b6a2d9f1baf7189ae4b3b412b3b7ed1c
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-04-08 18:28:56 +0900

    Create contect.js

---
+++ commands/contect.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+
+ module.exports.run = async (bot, message, args) => {
+ 
+   let owner = client.users.get("417571990820618250")
+		
+
+		if(!rUser) return message.channel.send("사용법 : ~문의 [문의사항] ");
+		let reason = args.join(" ").slice(22);
+		
+ let owner = client.users.get("417571990820618250")
+ 
+ let txt = new Discord.RichEmbed()
+ .setAuthor(`문의가 왔다! ${message.author}`)
+ .addField(`문의 내용`, `${reason}`)
+ 
+ owner.send(txt)
+	 	          message.channel.send(`
+                      
+문의가 완료되었습니다
+문의사항은 [ ${reason} ] 입니다.
+잘못보냈을경우 "취소합니다" 라고 다시 보내주세요.
+`
+);
+		
+	};
+	
+module.exports.help = {
+	name: "문의",
+}

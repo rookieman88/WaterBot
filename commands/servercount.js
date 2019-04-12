53fdf37cef9e4578101a71d3d670d432ea5ca886
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-04-12 17:48:05 +0900

    Create servercount.js

---
+++ commands/servercount.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+ module.exports.run = async (bot, message, args) => {
+ 
+		
+
+
+
+		
+ let owner = "417571990820618250"
+ let havea = "417123204469882890"
+ 
+
+ 
+ if (message.author.id === owner || havea) {
+
+let servercount = bot.guilds.size
+ 
+	 	          message.channel.send(`
+                      
+워터봇은 ${servercount} 개의 서버에 있습니다!
+`
+);
+ }
+		
+	};
+	
+module.exports.help = {
+	name: "서버",
+}

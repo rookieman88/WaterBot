87e72aac2b193ad6dd0545942d2b552b66feecf9
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-04-13 20:03:34 +0900

    Update translator.js

--- commands/translator.js
+++ commands/translator.js
@@ -8,29 +8,32 @@ module.exports.run = async (bot, message, args) => {
   var inputyee = args[2]
   
   
-let for = message.content.replace(`~번역`, "")
+let fo = message.content.replace(`~번역`, "")
 let output;
   
   
   if (input == '영어') {
    
-   translator(`${for}`, {to: 'en'}).then(res => {
+   translator(`${fo}`, {to: 'en'}).then(res => {
     output = (res.text);
     
     let calcembed = new Discord.RichEmbed()
     .setTitle("번역기")
-    .addField(`${for} 을 영어로`, `${output}`)
+    .addField(`${fo} 을 영어로`, `${output}`)
        message.channel.send(calcembed)
+   });
        
     } else if (input == '한국어') {
-   translator(`${for}`, {to: 'kr'}).then(res => {
+   translator(`${fo}`, {to: 'kr'}).then(res => {
     output = (res.text);
     
     let calcembed = new Discord.RichEmbed()
     .setTitle("번역기")
-    .addField(`${for} 을 한국어로`, `${output}`)
+    .addField(`${fo} 을 한국어로`, `${output}`)
        message.channel.send(calcembed)
+     });
     }
+                                        
   
 
 

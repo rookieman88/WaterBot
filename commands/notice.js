ab5cf4372b787dcdf87aaafdd0316e21bba53f8b
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-04-13 15:28:54 +0900

    Update notice.js

--- commands/notice.js
+++ commands/notice.js
@@ -9,30 +9,30 @@ const client = new Discord.Client();
     try {
       this.client.guilds.map((guild) => {
         let found = 0
-        let acc = guild.channels.find(`name`,`공지`)
         guild.channels.map((c) => {
           if (found === 0) {
             if (c.type === "text") {
               if (c.permissionsFor(this.client.user).has("VIEW_CHANNEL") === true) {
                 if (c.permissionsFor(this.client.user).has("SEND_MESSAGES") === true) {
-                  acc.send(`${reason}`);
+                  guild.channels.find(`name`,`welcome`).send(`${reason}`);
                   found = 1;
                 }
               }
             }
-          }
-        });
+	  }
+	}
       });
+
+    catch (err) {
+      console.log("공지 발신에 실패한 서버가 있습니다.");
     }
+	      
     message.channel.send(`
 공지 발신이 완료되었습니다
 공지 내용은 ${reason} 입니다.
 `)
-    catch (err) {
-      console.log("공지 발신에 실패한 서버가 있습니다.");
-    }
   } else {
-    message.reply("권한이 없습니다.")
+    message.reply("권한이 없습니다.");
   }
     
 		

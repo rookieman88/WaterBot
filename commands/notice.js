5b546e83e43d11eec2c60a39cfdb399f093a3fa9
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-04-13 19:49:13 +0900

    Update notice.js

--- commands/notice.js
+++ commands/notice.js
@@ -6,23 +6,24 @@ const client = new Discord.Client();
  let reason = message.content.replace(`~공지`, "")
 
  if (message.author.id === "417571990820618250") {
-bot.guilds.forEach((guild) => { //for each guild the bot is in
-         let defaultChannel = "";
+
+	 /*
+	    bot.guilds.forEach((guild) => { //for each guild the bot is in
+         let noticeChannel = "";
          guild.channels.forEach((channel) => {
-               if(channel.type == "text" && defaultChannel == "") {
+               if(channel.type == "text" && noticeChannel == "") {
                if(channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
-                   defaultChannel = channel;
+                   noticeChannel = channel;
                }
                }
          })
-	
-	defaultChannel.send(`${reason}`)
-
-message.channel(`
-  공지 발신 성공!
-  내용 : "${reason}"
-  `)
-} else {return message.channel.send("권한이 없습니다") }
+   }) 
+	 
+	 
+*/
+	 message.channel.send("공지")
+	 
+} else { message.channel.send("권한이 없습니다") }
     
 		
 	};

c9a3eb0900212c724ea5c0cde741a4e423f99d7a
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-04-13 15:55:42 +0900

    Update notice.js

--- commands/notice.js
+++ commands/notice.js
@@ -6,35 +6,54 @@ const client = new Discord.Client();
  let reason = message.content.replace(`~공지`, "")
 
  if (message.author.id === "417571990820618250") {
-    try {
-      let toSay = `${reason}`
-      this.client.guilds.map((guild) => {
-        let found = 0
-        guild.channels.map((c) => {
-          if (found === 0) {
-            if (c.type === "text") {
-              if (c.permissionsFor(this.client.user).has("VIEW_CHANNEL") === true) {
-                if (c.permissionsFor(this.client.user).has("SEND_MESSAGES") === true) {
-                  c.send(toSay);
-                  found = 1;
-                }
-              }
-            }
+let toSay = `${reason}`
+this.client.guilds.map((guild) => {
+  let found = 0
+  toSay = `${reason}`
+  let setting = guild.settings.get('공지')
+  if (setting === 'off') return
+  guild.channels.map((c) => {
+    if (found === 0) {
+      if (c.type === 'text') {
+        if (c.permissionsFor(this.client.user).has('VIEW_CHANNEL') === true) {
+          if (c.permissionsFor(this.client.user).has('SEND_MESSAGES') === true) {
+            c.send(toSay)
+            found = 1
           }
-        });
-      });
+        }
+      }
     }
-    catch (err) {
-      console.log("공지 발송 실패!");
+  })
+})
+message.reply(`Execution completed. Shouted "${toSay}"`)
+} else if (message.author.id === '251383432331001856') {
+let toSay = `${args.msg}
+~Chrono, SmoreSoftware Founder & Retired Developer`
+this.client.guilds.map((guild) => {
+  let found = 0
+  toSay = `${args.msg}
+~Chrono, SmoreSoftware Founder & Retired Developer
+Want to turn these announcements off? Do \`${guild.commandPrefix}settings add announcements off\` to opt out.`
+  let setting = guild.settings.get('announcements')
+  if (setting === 'off') return
+  guild.channels.map((c) => {
+    if (found === 0) {
+      if (c.type === 'text') {
+        if (c.permissionsFor(this.client.user).has('VIEW_CHANNEL') === true) {
+          if (c.permissionsFor(this.client.user).has('SEND_MESSAGES') === true) {
+            c.send(toSay)
+            found = 1
+          }
+        }
+      }
     }
-       
-   message.channel.send(`
-공지 발신이 완료되었습니다
-공지 내용은 ${reason} 입니다.
-`)
- } else {
-   message.reply("권한이 없습니다.");
-}
+  })
+})
+message.channel(`
+  공지 발신 성공!
+  내용 : "${toSay}"
+  `)
+} else {return message.channel.send("권한이 없습니다") }
     
 		
 	};

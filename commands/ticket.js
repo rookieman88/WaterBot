92fa200a5f09f1db09ef737a8933526dfe3385ec
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-04-14 20:05:20 +0900

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -65,38 +65,38 @@ if (Ran7 === Ran70) {
 		let mal;
 		
 if (itgo === 1) {
-	mal = `1개 당첨! [ +500 코인 (손익 : -500코인) ]`
-	let good = 500
-	  WatCoin[message.author.id] = {
-    WatCoin: sCoins + parseInt(good)
-  };
-} else if (itgo === 2) {
-	mal = `2개 당첨! [ +1000 코인 (손익 : 0코인) ]`
-	let good = 1000
-	  WatCoin[message.author.id] = {
-    WatCoin: sCoins + parseInt(good)
-  };
-} else if (itgo === 3) {
-		mal = `3개 당첨! [ +1500 코인 (손익 : +500코인) ]`
+	mal = `1개 당첨! [ +1500 코인 (손익 : +500코인) ]`
 	let good = 1500
 	  WatCoin[message.author.id] = {
     WatCoin: sCoins + parseInt(good)
   };
-} else if (itgo === 4) {
-		mal = `4개 당첨! [ +2000 코인 (손익 : +1000코인) ]`
+} else if (itgo === 2) {
+	mal = `2개 당첨! [ +2000 코인 (손익 : +1000코인) ]`
 	let good = 2000
 	  WatCoin[message.author.id] = {
     WatCoin: sCoins + parseInt(good)
   };
+} else if (itgo === 3) {
+		mal = `3개 당첨! [ +3000 코인 (손익 : +2000코인) ]`
+	let good = 3000
+	  WatCoin[message.author.id] = {
+    WatCoin: sCoins + parseInt(good)
+  };
+} else if (itgo === 4) {
+		mal = `4개 당첨! [ +4000 코인 (손익 : +3000코인) ]`
+	let good = 4000
+	  WatCoin[message.author.id] = {
+    WatCoin: sCoins + parseInt(good)
+  };
 } else if (itgo === 5) {
-		mal = `5개 당첨! [ +2500 코인 (손익 : +1500코인) ]`
-	let good = 2500
+		mal = `5개 당첨! [ +5000 코인 (손익 : +4000코인) ]`
+	let good = 5000
 	  WatCoin[message.author.id] = {
     WatCoin: sCoins + parseInt(good)
   };
 } else if (itgo === 6) {
-		mal = `6개 당첨! [ +3000 코인 (손익 : +2000코인) ]`
-	let good = 3000
+		mal = `6개 당첨! [ +6000 코인 (손익 : +5000코인) ]`
+	let good = 6000
 	  WatCoin[message.author.id] = {
     WatCoin: sCoins + parseInt(good)
   };

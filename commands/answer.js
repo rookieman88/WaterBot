175c7a34fa359ec49c4acfdc38822fa94ac311b5
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-04-20 19:40:33 +0900

    Update answer.js

--- commands/answer.js
+++ commands/answer.js
@@ -10,10 +10,15 @@ const bot = new Discord.Client();
 		
  let owner = "417571990820618250"
  let havea = "417123204469882890"
+ let chaemoong = '431085681847042048'
+ let remac = '361091925266137089'
+ let dorydarg = '405018851399565323'
+ let jenon = '550876882246303774'
+ let jenonp = '467625748631519244'
  
 
  
- if (message.author.id === owner || message.author.id === havea) {
+ if (message.author.id === owner || message.author.id === havea || message.author.id === chaemoong || message.author.id === remac || message.author.id === dorydarg || message.author.id === jenon || message.author.id === jenonp) {
 let pref = "~답변"
  let human = args[0]
   let User = bot.users.get(`${human}`)

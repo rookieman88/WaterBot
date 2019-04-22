14252847ceef8bd03a9787b0473a06b98053220c
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-04-22 19:59:29 +0900

    Update typevsp.js

--- commands/typevsp.js
+++ commands/typevsp.js
@@ -4,11 +4,11 @@ const bot = new Discord.Client();
 
  module.exports.run = async (bot, message, args) => {
  
-/*
+
 let ready = new Discord.RichEmbed()
 .setTitle('타자대결')
 .addField(`규칙`, `1. 1분을 기다립니다. \n 2. 3명 이상이 참가하면 시작합니다. \n 3. 렌덤으로 제시자가 주어집니다. \n 4. 제시자는 원하는 문장을 입력합니다. \n 5. 참가자들은 그 문장을 따라 적습니다. \n 6. 먼저 입력한 사람이 우승!`)   
-.setFooter('참가자를 기다리는중')   
+.setFooter('참가자를 기다리는중 (이모지를 눌러 참여하세요)')   
  message.channel.send(ready).then((msg) => {
    
 
@@ -23,14 +23,18 @@ let ready = new Discord.RichEmbed()
         let p2 = collected.array()[0].users.array()[2].id
         let p3 = collected.array()[0].users.array()[3].id
         
-	 let saram = let ranNum = Math.floor(Math.random() * (4)) + 0;
+	 let saram = let ranNum = Math.floor(Math.random() * (3)) + 0;
 	 
+	 let itgo;     
+	      
 	 if(saram === 1) {
-	 let itgo = p1
+	 itgo = p1
 	 }else if (saram === 2){
-	 let itgo = p2 
+	 itgo = p2 
 	 } else if (saram === 3){
-	 let itgo = p3
+	 itgo = p3
+	 } else {
+	 itgo = p3
 	 }
         
         let start = new Discord.RichEmbed()
@@ -39,13 +43,46 @@ let ready = new Discord.RichEmbed()
 	
 	msg.edit(start)
 	
+	 const filter = (m) => m.author.id === itgo;
+	 
+	     message.channel.awaitMessages(filter, {
+		max: 1,
+		time: 60000
+    }).then((collected) => {
+let titleinput = collected.first().content;
+
+        let gogo = new Discord.RichEmbed()
+        .setTitle('짜잔!')
+        .addField(`빨리 ${titleinput} 을 입력하세요!`)
+	
+	msg.edit(gogo)
+	
+ const fillter = (u) => titleinput === message.content && u.id !== itgo
+
+	     message.channel.awaitMessages(fillter, {
+		max: 1
+    }).then((collected) => {
+    
+    let nvm = new Discord.RichEmbed()
+    .setTitle('게임끝!')
+    .addField(`우승한사람`, collected.first().author.username)
+    
+    msg.edit(nvm)
+
+
+
+
+
+});
+	
 	
       });
    
-   });
+      });
+ });
          
    
-  */
+  
 		
 	};
 	

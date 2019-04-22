50fec345054764cbc48855fcbf17a0f51babb95f
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-04-22 19:28:54 +0900

    Update typevsp.js

--- commands/typevsp.js
+++ commands/typevsp.js
@@ -7,7 +7,7 @@ const bot = new Discord.Client();
 /*
 let ready = new Discord.RichEmbed()
 .setTitle('타자대결')
-.addField(`규칙`, `1. 1분을 기다립니다. \n 2. 3명 이상이 참가하면 시작합니다. \n 3. 렌덤으로 제시자가 주어집니다. \n 4. 제시자는 원하는 문장을 입력합니다. \n 5. 참가자들은 그 문장을 따라 적습니다. \n 6. 먼저 입력한 사람이 우승! \n 7. 3~6 반복`)   
+.addField(`규칙`, `1. 1분을 기다립니다. \n 2. 3명 이상이 참가하면 시작합니다. \n 3. 렌덤으로 제시자가 주어집니다. \n 4. 제시자는 원하는 문장을 입력합니다. \n 5. 참가자들은 그 문장을 따라 적습니다. \n 6. 먼저 입력한 사람이 우승!`)   
 .setFooter('참가자를 기다리는중')   
  message.channel.send(ready).then((msg) => {
    
@@ -17,17 +17,29 @@ let ready = new Discord.RichEmbed()
    
          const filter = (reaction, user) => reaction.emoji.name === '🎉' && user.id !== bot.user.id
       msg.awaitReactions(filter, {
-        maxUsers: 4
+        maxUsers: 3
 }).then((collected) => {
         let p1 = collected.array()[0].users.array()[1].id
         let p2 = collected.array()[0].users.array()[2].id
         let p3 = collected.array()[0].users.array()[3].id
-        let p4 = collected.array()[0].users.array()[4].id
         
+	 let saram = let ranNum = Math.floor(Math.random() * (4)) + 0;
+	 
+	 if(saram === 1) {
+	 let itgo = p1
+	 }else if (saram === 2){
+	 let itgo = p2 
+	 } else if (saram === 3){
+	 let itgo = p3
+	 }
         
         let start = new Discord.RichEmbed()
         .setTitle('시작')
-        .addField('')
+        .addField(`<@!${itgo}> 님! 문장을 입력해 주세요`)
+	
+	msg.edit(start)
+	
+	
       });
    
    });

6d5ab181da21f04be8f968e31fd1034d250f945c
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-04-24 07:23:06 +0900

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -5,13 +5,14 @@ if(message.guild.id === '542295644333801472') {
 let ver = message.guild.roles.find(`name`, "회원");
 
 
+    
 await(message.author.addRole(ver.id));
+
 
 message.channel.send(`
 ${message.author}
 인증이 완료되었습니다!
-')
-
+`)
 
 } else { return }
 

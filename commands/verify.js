c569c1c8baa60b22d1f7bde81c69ac3ed1689c29
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-04-24 07:29:40 +0900

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -1,12 +1,13 @@
 module.exports.run = async (bot, message, args) => {
 
 if(message.guild.id === '542295644333801472') {
-
-let ver = message.guild.roles.find(`name`, "회원");
+    
+    
+if (message.member.roles.has('570219053071859725')) return message.reply("이미 인증되셨습니다!");
 
 
     
-await(message.author.addRole(ver.id));
+await(message.member.addRole('570219053071859725'));
 
 
 message.channel.send(`

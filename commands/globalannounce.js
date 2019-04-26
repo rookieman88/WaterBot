682d4914db8f1bc511a7f2c60d205b2d17add2ce
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-04-26 20:11:35 +0900

    Create globalannounce.js

---
+++ commands/globalannounce.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+ module.exports.run = async (bot, message, args) => {
+ 
+		
+
+
+
+		
+ let owner = "417571990820618250"
+ 
+
+ 
+ if (message.author.id === owner) {
+let pref = "~공지 "
+
+bot.guilds.forEach(guild => {
+    guild.channels.find(t => t.name == '공지').send(pref);
+})
+		
+	};
+	
+module.exports.help = {
+	name: "공지",
+}

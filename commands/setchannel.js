ef0c73b1d03278a9267f9fca9a504f4000c7fae9
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-04-29 20:16:04 +0900

    Update setchannel.js

--- commands/setchannel.js
+++ commands/setchannel.js
@@ -94,6 +94,46 @@ let chaid = message.channel.id
 	
 } else if (args[0] === '공지') {
 	
+			superagent.get("https://api.myjson.com/bins/sztu0").then((res) => {
+		let welcomechannel = res.body;
+
+  if(!welcomechannel[message.guild.id]){
+        welcomechannel[message.guild.id] = {
+      welcomechannel: 0
+    };
+  }
+let msguild = welcomechannel[message.guild.id].welcomechannel		
+if (msguild === 0) {
+		
+
+let chaid = message.channel.id
+
+  welcomechannel[message.guild.id] = {
+    welcomechannel: chaid
+  };
+  
+  message.channel.send('공지 채널이 설정되었습니다.')
+
+
+} else if (msguild > 0) {
+	let chaid = message.channel.id
+ welcomechannel[message.guild.id] = {
+    welcomechannel: 0
+  };
+	
+	message.channel.send("인사 채널이 초기화되었습니다")
+	
+}
+
+
+ superagent.put("https://api.myjson.com/bins/sztu0").send(welcomechannel).catch((err) => console.log(err));
+
+  
+  
+
+
+});
+	
 	
 }
 }

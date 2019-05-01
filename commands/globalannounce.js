1e6a53455c672b56636a4209603d7b87633d9039
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-05-01 09:33:06 +0900

    Update globalannounce.js

--- commands/globalannounce.js
+++ commands/globalannounce.js
@@ -14,6 +14,8 @@ const superagent = require('superagent');
 
  
  if (message.author.id === owner) {
+	 
+	 if(args[0] === '에브리원') {
 
 	 superagent.get("https://api.myjson.com/bins/sztu0").then((res) => {
 let welcomechannel = res.body;
@@ -31,6 +33,40 @@ return
 	let cha = msguild
 	let ann = new Discord.RichEmbed()
 	.addField(`워터봇 공지`, `${reason}`)
+	.setColor(`#00ffc1`)
+	.setFooter('Oasics#5074 가 발신한 공지입니다.')
+let Ch = bot.channels.get(cha)
+	Ch.sendEmbed(ann)
+	Ch.send("everyone")
+
+})
+	 	 let reason = message.content.replace(`~공지 `, "")
+	 	message.channel.send(`
+발신이 완료되었습니다!
+공지 내용은 [ ${reason} + everyone ] 입니다.
+`)
+	 });
+		 
+	 } else {
+		 
+	
+	 superagent.get("https://api.myjson.com/bins/sztu0").then((res) => {
+let welcomechannel = res.body;
+
+bot.guilds.forEach(g => {
+	 let reason = message.content.replace(`~공지 `, "")
+	 
+	   if(!welcomechannel[g.id]){
+return
+}
+	let msguild = welcomechannel[g.id].welcomechannel	
+	if (msguild === 0) { return }
+
+	 
+	let cha = msguild
+	let ann = new Discord.RichEmbed()
+	.addField(`워터봇 공지`, `${reason}`)
+	.setColor(`#00ffc1`)
 	.setFooter('Oasics#5074 가 발신한 공지입니다.')
 let Ch = bot.channels.get(cha)
 	Ch.sendEmbed(ann)
@@ -41,7 +77,9 @@ let Ch = bot.channels.get(cha)
 발신이 완료되었습니다!
 공지 내용은 [ ${reason} ] 입니다.
 `)
-	 });
+	 });	 
+		 
+	 }
  } else {
 	 message.channel.send('당신은 봇 관리자로 등록되어있지 않습니다.')
  }
